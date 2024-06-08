d430d17c4e6bd4918487df20386fe828660177f9
Karina Osadchaya <dyach.zero@gmail.com>
2024-06-08 10:27:54 +0300

    ADD: change background color to mint

--- ViewController.swift
+++ ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(titleLabel)
+        view.backgroundColor = .systemMint
     }
 
 
