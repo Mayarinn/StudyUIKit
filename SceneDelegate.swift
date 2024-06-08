2faa505e0326897f6cfbb58e399e06781a62c300
Karina Osadchaya <dyach.zero@gmail.com>
2024-06-08 09:28:11 +0300

    ADD: set root viewController

--- SceneDelegate.swift
+++ SceneDelegate.swift
@@ -16,6 +16,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         
+        let viewController = ViewController()
+        window.rootViewController = viewController
+        window.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
