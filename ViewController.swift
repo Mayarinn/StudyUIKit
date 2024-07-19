1944126c1630fa9dd4b91ccf3a597e3e911c0333
Karina Osadchaya <dyach.zero@gmail.com>
2024-07-19 09:01:05 +0300

    DEL: Remove comments

--- ViewController.swift
+++ ViewController.swift
@@ -23,27 +23,9 @@ class ViewController: UIViewController {
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            titleLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            
         ])
-        
-//        let titleTopConstraint = view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20)
-//        titleTopConstraint.isActive = true
-        
-//        let titleConstraints = [
-//            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
-//        ]
-//        NSLayoutConstraint.activate(titleConstraints)
     }
-
-//    func const(){
-//         NSLayoutConstraint.activate([
-//            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-//            titleLabel.heightAnchor.constraint(equalToConstant: 50)
-//            ])
-//    }
 
 }
 
