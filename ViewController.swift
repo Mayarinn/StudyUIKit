d9ec36ee38515d7a086919c4a11f7136cb70a61b
Karina Osadchaya <dyach.zero@gmail.com>
2024-07-19 08:47:39 +0300

    FIX: Title label constraints

--- ViewController.swift
+++ ViewController.swift
@@ -9,20 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var titleLabel: UILabel {
+    var titleLabel: UILabel = {
         let titleLabel = UILabel()
         titleLabel.text = "Title"
         
         return titleLabel
-    }
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(titleLabel)
         view.backgroundColor = .systemMint
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        let titleTopConstraint = view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20)
-        titleTopConstraint.isActive = true
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            titleLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+//        let titleTopConstraint = view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20)
+//        titleTopConstraint.isActive = true
         
 //        let titleConstraints = [
 //            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
@@ -30,6 +38,12 @@ class ViewController: UIViewController {
 //        NSLayoutConstraint.activate(titleConstraints)
     }
 
+//    func const(){
+//         NSLayoutConstraint.activate([
+//            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+//            titleLabel.heightAnchor.constraint(equalToConstant: 50)
+//            ])
+//    }
 
 }
 
